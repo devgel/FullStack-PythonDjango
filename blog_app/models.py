ce043f81a9584686b4af6f0b6a923c0dfa9b6883
devgel <argelpolicarpio@gmail.com>
2018-11-18 20:32:15 +0800

    third commit

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
+    slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
