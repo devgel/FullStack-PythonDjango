e65a62ae52023c0eebef6a40dc535ffc215ca1a9
devgel <argelpolicarpio@gmail.com>
2018-11-18 20:17:11 +0800

    Second commit

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yd(r3ujk&n-rh8r=s!!*nd@m@4t_@&j)fd%w4z-uf9bxuj9ce('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1','rgelol.pythonanywhere.com']
 
 
 # Application definition
